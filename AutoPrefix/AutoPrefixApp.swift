79635d35c22633c79e6462bcec63fa677a745f08
Amir Bitaraf <bitaraf.amir@gmail.com>
2022-10-31 22:29:46 +0100

    Add XCode Project, Add Podfile

---
+++ AutoPrefix/AutoPrefixApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoPrefixApp.swift
+//  AutoPrefix
+//
+//  Created by Amir Bitaraf on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct AutoPrefixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '14.0'
+
+target 'AutoPrefix' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'libPhoneNumber-iOS'
+
+end
+
